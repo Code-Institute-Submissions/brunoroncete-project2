9f4f3c36cfbd5558e53b6d483a3a811ad5fdc166
brunoroncete <87933260+brunoroncete@users.noreply.github.com>
2021-12-12 21:04:52 +0000

    adding game html

---
+++ script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll('.fbflag');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.toggle('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
