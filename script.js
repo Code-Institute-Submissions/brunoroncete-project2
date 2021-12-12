fd2ddd569eb45d0d68100a35a50e10c335884f82
brunoroncete <87933260+brunoroncete@users.noreply.github.com>
2021-12-12 21:16:03 +0000

    making the cards flip

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ function flipCard() {
     } else {
         hasFlippedCard = false;
         secondCard = this;
-
+        if (firstCard.dataset.type === secondCard.dataset.type) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1500);
+        }
     }
 }
-
 cards.forEach(card => card.addEventListener('click', flipCard));
