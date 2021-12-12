d6a581a44d210435fa71a443d4789b840c30f61f
brunoroncete <87933260+brunoroncete@users.noreply.github.com>
2021-12-12 21:40:11 +0000

    fixing flipping bugs

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 const cards = document.querySelectorAll('.fbflag');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
-        if (firstCard.dataset.type === secondCard.dataset.type) {
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
+
+        return;
     }
+
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.type === secondCard.dataset.type;
+
+    isMatch ? disableCards() : unflipCards();
+
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+    }, 1000);
 }
 cards.forEach(card => card.addEventListener('click', flipCard));
